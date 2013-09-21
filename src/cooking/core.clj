5be6716eebfbc562b069d9cb950882b3faa2e30b
Chris Ford <christophertford@gmail.com>
2013-09-21 13:30:36 +0300

    Check the result.

--- src/cooking/core.clj
+++ src/cooking/core.clj
@@ -54,24 +54,50 @@
     (add-some-water {:time 0, :butterbeans 100})
       ;=> {:time 1, :butterbeans 100, :water 200}
 
+
+    (defn fry [minutes]
+      (fn [dish]
+        (update-in
+          (assoc dish :temperature 50)
+          [:water]
+          (plus (- minutes)))))
+
+    (def room-temperature 21)
+
     (defn sit [minutes]
       (fn [dish]
-        (mix-in dish :time minutes))) 
+        (let [temperature (max
+                            (- (:temperature dish) (* 2 minutes))
+                            room-temperature)]
+        (mix-in (assoc dish :temperature temperature) :time minutes)))) 
     
     (defn water-for [ingredient]
       (fn [dish]
         (let [quantity (* 2 (ingredient dish))]
           ((add :water quantity 3) dish))))
 
-    (def drain
+    (defn soak [ingredient minutes]
+      (fn [dish]
+        (let [absorbtion (/ (:water dish) 2)
+              absorb (comp
+                       (add :water (- absorbtion) 0)
+                       (add ingredient absorbtion minutes))]
+         (absorb dish))))
+
+    (defn drain []
       (fn [dish]
         (mix-in (dissoc dish :water) :time 3)))
-    
+
     (def recipe
       [(add :beans 150 1)
        (water-for :beans)
-       (sit (* 12 60))
-       drain])
+       (soak :beans (* 4 60))
+       (drain)
+       (add :water 50 1)
+       (add :garlic 5 1)
+       (fry 5)
+       (add :olive-oil 5 1)
+       (sit 10)])
 
     (defn preparations [steps]
       (let [perform (fn [dish step] (step dish))]
---
+++ test/cooking/core_test.clj
@@ -0,0 +1,9 @@
+(ns cooking.core-test
+  (:use clojure.test
+        cooking.core))
+
+(deftest check-result 
+  (testing
+    (is (=
+         (prepare recipe)
+         {:olive-oil 5, :temperature 30, :garlic 5, :water 45, :beans 300, :time 260}))))
